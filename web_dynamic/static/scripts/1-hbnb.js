e7fdc2f763bfe7f556e670d82e6538f1c86da2e7
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-02-05 16:43:46 +0100

    updated jquery for displaying amenities

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
   let selectedAmenities = [];
+  let selectedAmenitiesNames = [];
   $(document).on('change', 'input[type="checkbox"]', function() {
     let amenityID = $(this).data('id');
+    let amenityName = $(this).data('name');
     if ($(this).prop('checked')) {
       selectedAmenities.push(amenityID);
+      selectedAmenitiesNames.push(amenityName);
     } else {
       selectedAmenities = selectedAmenities.filter(id => id !== amenityID);
+      selectedAmenitiesNames = selectedAmenitiesNames.filter(name => name !== amenityName);
     }
-    $('div.Amenities h4').text('Selected Amenities: ' + selectedAmenities.join(', '));
+    $('div.amenities h4').text(selectedAmenitiesNames.join(', '));
   });
 });
 
