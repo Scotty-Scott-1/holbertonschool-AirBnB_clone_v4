40d2493b25b3755960d507a426cc77bf3636cba6
Scotty-Scott-1 <swilliamfr@outlook.com>
2024-02-06 10:40:08 +0100

    added files for task 2

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  let selectedAmenities = [];
+  let selectedAmenitiesNames = [];
+  let selectedAmenitiesText = "";
+  $(document).on('change', 'input[type="checkbox"]', function() {
+    let amenityID = $(this).data('id');
+    let amenityName = $(this).data('name');
+    if ($(this).prop('checked')) {
+      selectedAmenities.push(amenityID);
+      selectedAmenitiesNames.push(amenityName);
+    } else {
+      selectedAmenities = selectedAmenities.filter(id => id !== amenityID);
+      selectedAmenitiesNames = selectedAmenitiesNames.filter(name => name !== amenityName);
+    }
+    console.log(selectedAmenitiesNames.length);
+    if (selectedAmenitiesNames.length > 3) {
+      selectedAmenitiesText = `${selectedAmenitiesNames[0]}, ${selectedAmenitiesNames[1]}, ${selectedAmenitiesNames[2]}, ${selectedAmenitiesNames[3][0]}...`;
+    } else {
+      selectedAmenitiesText = selectedAmenitiesNames.join(', ');
+    }
+    $('div.amenities h4').text(selectedAmenitiesText);
+  });
+});
